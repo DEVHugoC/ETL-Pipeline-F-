e577dfce6029b729f164d734fbaae9279735b85e
DEVHugoC <hugodanielcajacuri@gmail.com>
2025-04-11 02:50:12 -0500

    Modificacion

--- copiar_bd.sh
+++ copiar_bd.sh
@@ -9,8 +9,13 @@ RUTA_ORIGEN="/var/jenkins_home/workspace/ProyectoFinal/bd_Ordenes_de_Compra.db"
 # Ruta destino dentro del contenedor (una carpeta que existe)
 RUTA_DESTINO="/var/jenkins_home/RESULTADO"
 
-# Crear carpeta si no existe
-mkdir -p "$RUTA_DESTINO"
-
-# Copiar archivo dentro del contenedor
+# Copiar el archivo dentro del contenedor a la carpeta montada
+echo "Copiando base de datos dentro del contenedor..."
 cp "$RUTA_ORIGEN" "$RUTA_DESTINO/bd_Ordenes_de_Compra.db"
+
+if [ $? -eq 0 ]; then
+    echo "Base de datos copiada con éxito al volumen compartido."
+else
+    echo "Error al copiar la base de datos dentro del contenedor."
+    exit 1
+fi
