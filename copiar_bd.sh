0458e3112d4c517212dd5299233504b798a82189
DEVHugoC <hugodanielcajacuri@gmail.com>
2025-04-11 03:15:59 -0500

    Modificacion 3

--- copiar_bd.sh
+++ copiar_bd.sh
@@ -6,15 +6,15 @@ CONTENEDOR="jenkins"
 # Ruta origen dentro del contenedor
 RUTA_ORIGEN="/var/jenkins_home/RESULTADO/bd_Ordenes_de_Compra.db"
 
-# Ruta destino en tu máquina local (en formato compatible con Bash)
-RUTA_DESTINO="/c/Users/Intel/Desktop/RESULTADO/bd_Ordenes_de_Compra.db"
+# Ruta destino dentro del contenedor, pero apuntando a la carpeta montada desde tu máquina
+RUTA_DESTINO="/var/jenkins_home/PROY_PY_CERTUS/bd_Ordenes_de_Compra.db"
 
-echo "Copiando base de datos a la máquina local..."
+echo "Copiando base de datos al volumen compartido..."
 
 docker cp "$CONTENEDOR:$RUTA_ORIGEN" "$RUTA_DESTINO"
 
 if [ $? -eq 0 ]; then
-    echo "Base de datos copiada con éxito."
+    echo "Base de datos copiada con éxito al volumen compartido."
 else
     echo "Error al copiar la base de datos."
     exit 1
